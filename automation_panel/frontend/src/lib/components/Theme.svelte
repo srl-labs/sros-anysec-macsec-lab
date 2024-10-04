dc340c1714df91cee5f201b5600e76b9de812511
Roman Dodin <dodin.roman@gmail.com>
2024-10-04 12:35:37 +0300

    added padding to the toggle switch and added width/height for the iframe

--- automation_panel/frontend/src/lib/components/Theme.svelte
+++ automation_panel/frontend/src/lib/components/Theme.svelte
@@ -12,7 +12,7 @@
 
 			themeIconSwitch();
 		}
-	})
+	});
 
 	const themeIconSwitch = () => {
 		if (darkMode) {
@@ -24,22 +24,56 @@
 			document.getElementById('toggle-dark-icon')?.classList.remove('hidden');
 			document.getElementById('toggle-light-icon')?.classList.add('hidden');
 		}
-	}
+	};
 
 	const toggleDarkMode = () => {
 		darkMode = !darkMode;
 		localStorage.setItem('theme', darkMode ? 'dark' : 'light');
 		themeIconSwitch();
-	}
+	};
 </script>
 
-<button class="flex w-8 h-5 ml-2 rounded-full items-center transition duration-30 {darkMode ? 'bg-gray-500' : 'bg-gray-300'}" on:click={toggleDarkMode}>
-  <div id="switch-toggle" class="w-5 h-5 p-1 relative rounded-full transition duration-500 transform {darkMode ? 'bg-gray-600 translate-x-3' : 'bg-white border border-gray-300 -translate-x-0'}">
-    <svg id="toggle-dark-icon" class="text-black" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"/>
-    </svg>
-    <svg id="toggle-light-icon" class="text-white hidden" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"/>
-    </svg>
-  </div>
+<button
+	class="flex w-8 h-5 ml-2 rounded-full items-center transition duration-30 {darkMode
+		? 'bg-gray-500'
+		: 'bg-gray-300'}"
+	on:click={toggleDarkMode}
+>
+	<div
+		id="switch-toggle"
+		class="w-5 h-5 p-1 relative rounded-full transition duration-500 transform {darkMode
+			? 'bg-gray-600 translate-x-3'
+			: 'bg-white border border-gray-300 -translate-x-0'}"
+	>
+		<svg
+			id="toggle-dark-icon"
+			class="text-black"
+			xmlns="http://www.w3.org/2000/svg"
+			fill="none"
+			viewBox="0 0 24 24"
+			stroke="currentColor"
+		>
+			<path
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				stroke-width="2"
+				d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
+			/>
+		</svg>
+		<svg
+			id="toggle-light-icon"
+			class="text-white hidden"
+			xmlns="http://www.w3.org/2000/svg"
+			fill="none"
+			viewBox="0 0 24 24"
+			stroke="currentColor"
+		>
+			<path
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				stroke-width="2"
+				d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+			/>
+		</svg>
+	</div>
 </button>
--- automation_panel/frontend/src/routes/+page.svelte
+++ automation_panel/frontend/src/routes/+page.svelte
@@ -18,7 +18,7 @@
 		document.getElementById('close-sidebar')?.classList.toggle('hidden');
 	};
 
-	const packetCaptureLinks = ["PE1-eth1", "PE1-eth2"]
+	const packetCaptureLinks = ['PE1-eth1', 'PE1-eth2'];
 	const serviceOptions = ['VLL', 'VPLS', 'VPRN'];
 	const link = [
 		{ id: 'top', title: 'Top Link', src: 'PE1', dest: 'P3' },
@@ -30,11 +30,11 @@
 	];
 
 	function edgesharkLink(link: string) {
-		let [neName, ifcName] = link.split("-")
-		let baseUrl = "packetflix:ws://devbox:5001/capture?"
-		let urlParams = `container={"network-interfaces":["${ifcName}"],"name":"${neName.toLocaleLowerCase()}","type":"docker","prefix":""}&nif=${ifcName}`
-		return baseUrl + urlParams
-	} 
+		let [neName, ifcName] = link.split('-');
+		let baseUrl = 'packetflix:ws://devbox:5001/capture?';
+		let urlParams = `container={"network-interfaces":["${ifcName}"],"name":"${neName.toLocaleLowerCase()}","type":"docker","prefix":""}&nif=${ifcName}`;
+		return baseUrl + urlParams;
+	}
 
 	const updateToggle = (state: AllState) => {
 		const toggle = (section: string, data: ServiceState | LinkState) => {
@@ -95,35 +95,60 @@
 </svelte:head>
 
 <nav class="fixed w-screen top-0 z-30 text-sm font-nunito">
-	<div class="flex items-center justify-between p-4 bg-white dark:bg-gray-800 border-b border-gray-200 dark:border-gray-700">
+	<div
+		class="flex items-center justify-between p-4 bg-white dark:bg-gray-800 border-b border-gray-200 dark:border-gray-700"
+	>
 		<div class="flex items-center">
 			<button type="button" class="pr-4 dark:text-gray-200 lg:hidden" on:click={toggleSidebar}>
-        <svg id="open-sidebar" class="w-5 h-5" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 25 25" xmlns="http://www.w3.org/2000/svg">
-          <path d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25H12"></path>
-        </svg>
-        <svg id="close-sidebar" class="w-5 h-5 hidden" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 25 25" xmlns="http://www.w3.org/2000/svg">
-          <path d="M6 18L18 6M6 6l12 12"></path>
-        </svg>
-      </button>
-			<div ><img src="/images/navbar-logo.png" alt="Logo" width="25" /></div>
+				<svg
+					id="open-sidebar"
+					class="w-5 h-5"
+					fill="none"
+					stroke="currentColor"
+					stroke-width="2"
+					viewBox="0 0 25 25"
+					xmlns="http://www.w3.org/2000/svg"
+				>
+					<path d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25H12"></path>
+				</svg>
+				<svg
+					id="close-sidebar"
+					class="w-5 h-5 hidden"
+					fill="none"
+					stroke="currentColor"
+					stroke-width="2"
+					viewBox="0 0 25 25"
+					xmlns="http://www.w3.org/2000/svg"
+				>
+					<path d="M6 18L18 6M6 6l12 12"></path>
+				</svg>
+			</button>
+			<div><img src="/images/navbar-logo.png" alt="Logo" width="25" /></div>
 		</div>
 		<div class="md:flex text-center">
 			<p class="dark:text-gray-200">ANYSec Lab</p>
 			<p class="dark:text-gray-200">&nbsp;Automation Panel</p>
 		</div>
-		<div class="flex items-center">
+		<div class="flex pr-4 items-center">
 			<Theme />
 		</div>
 	</div>
 </nav>
 
 <div class="flex text-sm font-nunito">
-	<aside id="sidebar" class="h-screen fixed lg:sticky top-0 left-0 w-[300px] pt-[73px] md:pt-[58px] bg-white dark:bg-gray-800 border-r border-gray-200 dark:border-gray-700 transition-transform duration-300 -translate-x-full lg:-translate-x-0">
+	<aside
+		id="sidebar"
+		class="h-screen fixed lg:sticky top-0 left-0 w-[300px] pt-[73px] md:pt-[58px] bg-white dark:bg-gray-800 border-r border-gray-200 dark:border-gray-700 transition-transform duration-300 -translate-x-full lg:-translate-x-0"
+	>
 		<div class="flex flex-col h-full">
 			<div class="flex-grow overflow-y-auto scroll-light dark:scroll-dark">
 				<div class="p-4 space-y-4">
 					<div class="rounded-lg border border-gray-200 dark:border-gray-600">
-						<p class="px-4 py-2 text-center rounded-t-lg font-bold text-gray-900 dark:text-white bg-gray-200 dark:bg-gray-700 border-b border-gray-200 dark:border-gray-600">ICMP State</p>
+						<p
+							class="px-4 py-2 text-center rounded-t-lg font-bold text-gray-900 dark:text-white bg-gray-200 dark:bg-gray-700 border-b border-gray-200 dark:border-gray-600"
+						>
+							ICMP State
+						</p>
 						<div class="py-3 space-y-3">
 							{#each formInputs as entry}
 								<Form {entry} />
@@ -133,7 +158,11 @@
 						</div>
 					</div>
 					<div class="rounded-lg border border-gray-200 dark:border-gray-600 md:min-w-40">
-						<p class="px-4 py-2 text-center rounded-t-lg font-bold text-gray-900 dark:text-white bg-gray-200 dark:bg-gray-700 border-b border-gray-200 dark:border-gray-600">Link State</p>
+						<p
+							class="px-4 py-2 text-center rounded-t-lg font-bold text-gray-900 dark:text-white bg-gray-200 dark:bg-gray-700 border-b border-gray-200 dark:border-gray-600"
+						>
+							Link State
+						</p>
 						<div class="py-3 space-y-3">
 							{#each link as entry}
 								<div class="flex items-center justify-between px-4">
@@ -145,8 +174,16 @@
 									</div>
 									<div class="flex">
 										<label class="inline-flex items-center cursor-pointer">
-											<input type="checkbox" class="sr-only peer" id="link-{entry.id}" checked={allState.link[entry.id]} on:change={(event) => triggerSet(event, 'link', entry.id)} />
-											<div class="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-2 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+											<input
+												type="checkbox"
+												class="sr-only peer"
+												id="link-{entry.id}"
+												checked={allState.link[entry.id]}
+												on:change={(event) => triggerSet(event, 'link', entry.id)}
+											/>
+											<div
+												class="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-2 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"
+											></div>
 										</label>
 									</div>
 								</div>
@@ -154,20 +191,32 @@
 						</div>
 					</div>
 					<div class="rounded-lg border border-gray-200 dark:border-gray-600 md:min-w-40">
-						<p class="px-4 py-2 text-center rounded-t-lg font-bold text-gray-900 dark:text-white bg-gray-200 dark:bg-gray-700 border-b border-gray-200 dark:border-gray-600">ANYSec State</p>
+						<p
+							class="px-4 py-2 text-center rounded-t-lg font-bold text-gray-900 dark:text-white bg-gray-200 dark:bg-gray-700 border-b border-gray-200 dark:border-gray-600"
+						>
+							ANYSec State
+						</p>
 						<div class="py-3 space-y-3">
 							<Services key="anysec" services={serviceOptions} state={allState.anysec} />
 						</div>
 					</div>
 					<div class="rounded-lg border border-gray-200 dark:border-gray-600 md:min-w-40">
-						<p class="px-4 py-2 text-center rounded-t-lg font-bold text-gray-900 dark:text-white bg-gray-200 dark:bg-gray-700 border-b border-gray-200 dark:border-gray-600">Packet Capture</p>
+						<p
+							class="px-4 py-2 text-center rounded-t-lg font-bold text-gray-900 dark:text-white bg-gray-200 dark:bg-gray-700 border-b border-gray-200 dark:border-gray-600"
+						>
+							Packet Capture
+						</p>
 						<div class="flex flex-col space-y-4 p-4">
 							{#each packetCaptureLinks as entry}
-								{@const [neName, ifcname] = entry.split("-")}
-								<a href="{edgesharkLink(entry)}" target="_blank" class="px-3 py-1 flex items-center justify-between rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 border dark:border-gray-600">
+								{@const [neName, ifcname] = entry.split('-')}
+								<a
+									href={edgesharkLink(entry)}
+									target="_blank"
+									class="px-3 py-1 flex items-center justify-between rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 border dark:border-gray-600"
+								>
 									<span>{neName}</span>
 									<span>{ifcname}</span>
-									<span class="w-6"><img src="/images/edgeshark.png" alt="fin"/></span>
+									<span class="w-6"><img src="/images/edgeshark.png" alt="fin" /></span>
 								</a>
 							{/each}
 						</div>
@@ -178,6 +227,9 @@
 	</aside>
 	<main class="flex-1 bg-gray-400 pt-[73px] md:pt-[58px]">
 		<!-- svelte-ignore a11y-missing-attribute -->
-		<iframe src="http://{urlHost}:3000/d/kawVPD-Gk/anysec-telemetry"></iframe>
+		<iframe
+			class="h-[calc(100vh-55px)] w-full"
+			src="http://{urlHost}:3000/d/kawVPD-Gk/anysec-telemetry"
+		></iframe>
 	</main>
 </div>
